ce6a71968c2b2c24a8c969fa4b9fd24a3fdfda25
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2020-03-01 23:18:09 +0000

    Giving context to cb,

--- src/api/shop.js
+++ src/api/shop.js
@@ -6,6 +6,7 @@ import _products from './products.json'
 const TIMEOUT = 100
 
 export default {
+  //cb is nodeJS representation for a callback
   getProducts: (cb, timeout) => setTimeout(() => cb(_products), timeout || TIMEOUT),
   buyProducts: (payload, cb, timeout) => setTimeout(() => cb(), timeout || TIMEOUT)
 }
